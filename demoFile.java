5c5f8641312ce6d826c42ce3a2a36c4cf2c4909e
raman2021 <a00244354@mycambrian.ca>
2021-09-26 17:56:44 -0400

    Initial commit

---
+++ demoFile.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class demoFile{
+	
+	public static void main(String args[]){
+		
+		System.out.print("Enter a number: ");
+		Scanner sc = new Scanner(System.in);
+		
+		double num1 = sc.nextDouble();
+		
+		System.out.println("the number you entered is: " + num1);
+		
+	}
+}
